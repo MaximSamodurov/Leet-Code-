3913c751d2eef068422bfe1552c58a2695c42d73
Fixed <trueforce.go@gmail.com>
2023-04-17 20:30:28 +0400

    findMiddleIndex

--- Find the Middle Index in Array.playground/Contents.swift
+++ Find the Middle Index in Array.playground/Contents.swift
@@ -10,7 +10,21 @@ import UIKit
 //Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
 
 func findMiddleIndex(_ nums: [Int]) -> Int {
-        
     
-    return
+    var rightSum = nums.reduce(0, +)
+    var leftSum = 0
+    
+    for i in 0..<nums.count {
+        if leftSum != rightSum {
+            leftSum += nums[i] 
+            rightSum -= nums[i]
+            print(rightSum)
+        } else {
+            return i
+        }
+        
+    }
+    return -1
 }
+
+findMiddleIndex([2,3,-1,8,4])
