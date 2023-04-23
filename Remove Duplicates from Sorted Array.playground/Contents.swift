ad29b1e8d46b3f179e6a6997c6bc96e5c56b7488
Fixed <trueforce.go@gmail.com>
2023-04-23 15:10:45 +0400

    Remove Duplicates from sorted Array

--- Remove Duplicates from Sorted Array.playground/Contents.swift
+++ Remove Duplicates from Sorted Array.playground/Contents.swift
@@ -1,7 +1,14 @@
 import UIKit
 
 func removeDuplicates(_ nums: inout [Int]) -> Int {
-        
-    
+    let unique = Array(Set(nums))
+    let sorted = unique.sorted()
+    nums = sorted
+    return nums.count
     
    }
+
+
+
+var array = [1,2,2,3]
+print(removeDuplicates(&array))
