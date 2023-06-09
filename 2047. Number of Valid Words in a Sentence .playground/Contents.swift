e74d5322ed545042a9937273c2e55f3a7a5d16a9
Fixed <trueforce.go@gmail.com>
2023-06-09 12:25:11 +0400

    Number of valid sentence

---
+++ 2047. Number of Valid Words in a Sentence .playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+    func interpret(_ command: String) -> String {
+        command.replacingOccurrences(of: "()", with: "o").replacingOccurrences(of: "(al)", with: "al")
+    }
+
+
+interpret("G()(al)")
