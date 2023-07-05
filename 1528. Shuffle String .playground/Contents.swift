e28088793fa496b4eed91a4cd4004ace0c6aecb4
Fixed <trueforce.go@gmail.com>
2023-07-05 15:54:55 +0400

    2520. Count the Digits That Divide a Number

--- 1528. Shuffle String .playground/Contents.swift
+++ 1528. Shuffle String .playground/Contents.swift
@@ -1,19 +1,19 @@
 import UIKit
 
+
 func restoreString(_ s: String, _ indices: [Int]) -> String {
-    var dict: Dictionary = [Int : String]()
+    
     var res = ""
+    var dict = [Int:String]()
     
     for (key, value) in s.enumerated() {
         dict[indices[key]] = String(value)
     }
     
-    for key in dict.keys.sorted() {
-        res += dict[key]!
+    for i in dict.keys.sorted() {
+        res += dict[i]!
     }
     
     return res
-
 }
-
 restoreString("codeleet", [4,5,6,7,0,2,1,3])
