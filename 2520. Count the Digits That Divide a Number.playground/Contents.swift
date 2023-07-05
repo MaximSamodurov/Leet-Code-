f18c931c22f958d3b37138f40f388d69b1c78f3f
Fixed <trueforce.go@gmail.com>
2023-07-05 15:55:02 +0400

    2520. Count the Digits That Divide a Number

---
+++ 2520. Count the Digits That Divide a Number.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func countDigits(_ num: Int) -> Int {
+    var count = 0
+    
+    let numString = String(num)
+    
+    for char in numString {
+        print(char)
+        if num % Int(String(char))! == 0 {
+            count += 1
+        }
+    }
+    return count
+}
+
+countDigits(1248)
