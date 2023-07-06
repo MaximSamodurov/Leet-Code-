0364dfb0390a6a32ff9eb69c3af10b4aec17d2dc
Fixed <trueforce.go@gmail.com>
2023-07-06 14:32:54 +0400

    2535. Difference Between Element Sum and Digit Sum of an Array

--- 2520. Count the Digits That Divide a Number.playground/Contents.swift
+++ 2520. Count the Digits That Divide a Number.playground/Contents.swift
@@ -1,17 +1,20 @@
 import UIKit
 
-func countDigits(_ num: Int) -> Int {
-    var count = 0
-    
-    let numString = String(num)
-    
-    for char in numString {
-        print(char)
-        if num % Int(String(char))! == 0 {
-            count += 1
-        }
-    }
-    return count
-}
+//func countDigits(_ num: Int) -> Int {
+//    var count = 0
+//
+//    let numString = String(num)
+//
+//    for char in numString {
+//        print(char)
+//        if num % Int(String(char))! == 0 {
+//            count += 1
+//        }
+//    }
+//    return count
+//}
 
+func countDigits(_ num: Int) -> Int {
+    String(num).filter { Int(String($0))! % num == 0 }
+}
 countDigits(1248)
